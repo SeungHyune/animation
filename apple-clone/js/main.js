f2e76fe77b8aa88be91869ba02475961f3126048
seunghyune <114329713+SeungHyune@users.noreply.github.com>
2023-08-27 23:41:34 +0900

    apple-clone

--- apple-clone/js/main.js
+++ apple-clone/js/main.js
@@ -1,4 +1,9 @@
 (() => {
+
+    let yOffset = 0; // window.pageYOffset 대신 쓸 변수
+    let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
+    let currentScene = 0; // 현재 활성화된(눈 앞에 보고있는) 씬(scroll-section)
+
     const sceneInfo = [
         {
             // 0
@@ -38,6 +43,25 @@
         },
     ];
 
+    function scrollLoop(){
+        prevScrollHeight = 0;
+        for(let i=0;i<currentScene;i++){
+            prevScrollHeight += sceneInfo[i].scrollHeight;
+        }
+
+        if(yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight){
+            currentScene++;
+            document.body.setAttribute('id', `show-scene-${currentScene}`);
+        }
+        if(yOffset < prevScrollHeight){
+            if(currentScene === 0) return;
+            currentScene--;
+            document.body.setAttribute('id', `show-scene-${currentScene}`);
+        }
+
+        
+    }
+
 
     function setLayout(){
         // 각 스크롤 세션 높이 세팅
@@ -45,7 +69,25 @@
             sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
             sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
         }
+
+        yOffset = window.pageYOffset;
+
+        let totalScrollHeight = 0;
+        for(let i=0;i<sceneInfo.length;i++){
+            totalScrollHeight += sceneInfo[i].scrollHeight;
+            if(totalScrollHeight >= yOffset){
+                currentScene = i;
+                break;
+            }
+        }
+        document.body.setAttribute('id', `show-scene-${currentScene}`);
     }
 
+    window.addEventListener('scroll', () => {
+        yOffset = window.pageYOffset;
+        scrollLoop();
+    });
+
+    window.addEventListener('load', setLayout);
     window.addEventListener('resize', setLayout);
 })();
