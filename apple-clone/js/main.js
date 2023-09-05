f9cbbf609d23f904400f1650137f26ba1edad9a2
seunghyune <114329713+SeungHyune@users.noreply.github.com>
2023-09-05 23:17:05 +0900

    apple-clone

--- apple-clone/js/main.js
+++ apple-clone/js/main.js
@@ -4,6 +4,10 @@
     let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
     let currentScene = 0; // 현재 활성화된(눈 앞에 보고있는) 씬(scroll-section)
     let enterNewScene = false;
+    let acc = 0.1;
+    let delayedYOffset = 0;
+    let rafId;
+    let rafState;
 
     const sceneInfo = [
         {
@@ -144,7 +148,6 @@
             sceneInfo[3].objs.images.push(imgElem3);
         }
     }
-    setCanvasImages();
 
     function calcValues(values, currentYOffset){
         let rv;
@@ -179,8 +182,8 @@
         switch (currentScene) {
             case 0:
                 // console.log('0 play');
-                let sequence = Math.round(calcValues(values.imageSequence, currentYOffset));
-                objs.context.drawImage(objs.videoImages[sequence],0,0);
+                // let sequence = Math.round(calcValues(values.imageSequence, currentYOffset));
+                // objs.context.drawImage(objs.videoImages[sequence],0,0);
                 objs.canvas.style.opacity = calcValues(values.canvas_opacity, currentYOffset);
 
                 if (scrollRatio <= 0.22) {
@@ -227,8 +230,8 @@
     
             case 2:
                 // console.log('2 play');
-                let sequence2 = Math.round(calcValues(values.imageSequence, currentYOffset));
-                objs.context.drawImage(objs.videoImages[sequence2],0,0);
+                // let sequence2 = Math.round(calcValues(values.imageSequence, currentYOffset));
+                // objs.context.drawImage(objs.videoImages[sequence2],0,0);
 
                 if(scrollRatio <= 0.5) {
                     // in
@@ -412,12 +415,12 @@
             prevScrollHeight += sceneInfo[i].scrollHeight;
         }
 
-        if(yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight){
+        if(delayedYOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight){
             enterNewScene = true;
             currentScene++;
             document.body.setAttribute('id', `show-scene-${currentScene}`);
         }
-        if(yOffset < prevScrollHeight){
+        if(delayedYOffset < prevScrollHeight){
             enterNewScene = true;
             if(currentScene === 0) return;
             currentScene--;
@@ -465,15 +468,52 @@
         sceneInfo[2].objs.canvas.style.transform = `translate3d(-50%,-50%,0) scale(${heightRatio})`;
     }
 
+    function loop(){
+        delayedYOffset = delayedYOffset + (yOffset - delayedYOffset) * acc;
+
+        if(!enterNewScene) {
+            
+            if(currentScene === 0 || currentScene === 2) {
+                const currentYOffset = delayedYOffset - prevScrollHeight;
+                const objs = sceneInfo[currentScene].objs;
+                const values = sceneInfo[currentScene].values;
+                let sequence = Math.round(calcValues(values.imageSequence, currentYOffset));
+                if(objs.videoImages[sequence]) {
+                    objs.context.drawImage(objs.videoImages[sequence],0,0);
+                }
+            }
+        }
+
+        rafId = requestAnimationFrame(loop);
+
+        if(Math.abs(yOffset - delayedYOffset) < 1) {
+            cancelAnimationFrame(rafId);
+            rafState = false;
+        }
+    }
+
     window.addEventListener('scroll', () => {
         yOffset = window.pageYOffset;
         scrollLoop();
         checkMenu();
+
+        if(!rafState) {
+            raf = requestAnimationFrame(loop);
+            rafState = true;
+        }
     });
 
     window.addEventListener('load', () => {
         setLayout();
         sceneInfo[0].objs.context.drawImage(sceneInfo[0].objs.videoImages[0], 0, 0);
     });
-    window.addEventListener('resize', setLayout);
+    window.addEventListener('resize', () => {
+        if(window.innerWidth > 900) {
+            setLayout();
+        }
+        sceneInfo[3].values.rectStartY = 0;
+    });
+    window.addEventListener('orientationchange', setLayout);
+
+    setCanvasImages();
 })();
